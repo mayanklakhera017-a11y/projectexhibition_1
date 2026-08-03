091e473250c2170e66041823fd28c287a76c59a5
Mayank <mayanklakhera017@gmail.com>
2026-08-04 00:23:59 +0530

    Added a class five tuple for ip and protocol

--- pkttyp.java
+++ pkttyp.java
@@ -14,6 +14,41 @@ public class pkttyp
         private String dstIp;
         private int scrPort;
         private int dstPort;
+        private Protocol protocol;
+
+        public fiveTuple(String scrIp, String dstIp, int scrPort, int dstPort, Protocol protocol)
+        {
+            this.scrIp = scrIp;
+            this.dstIp = dstIp;
+            this.scrPort = scrPort;
+            this.dstPort = dstPort;
+            this.protocol = protocol;
+        }
+
+        public String getSrcIP()
+        {
+            return scrIp;
+        }
+
+        public String getDstIP()
+        {
+            return dstIp;
+        }
+
+        public int getSrcPort()
+        {
+            return scrPort;
+        }
+
+        public int getDstPort()
+        {
+            return dstPort;
+        }
+
+        public Protocol getProtocol()
+        {
+            return protocol;
+        }
     }
 }
 
